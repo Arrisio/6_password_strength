1b952909e6000a7b0b1464314bef2ce5bf3bdc07
dv <suchkov.dv@gmail.com>
2018-06-01 23:18:48 +0300

    release

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-# Password Strength Calculator
+# Определение сложности пароля
 
-[TODO. There will be project description]
+Программа вычисляет сложность пароля. Ввод пароля для анализа осуществляется через командную строку.
+Программа выдаёт ему оценку от 1 до 10. 1 – очень слабый пароль, 10 – очень крутой.
 
-# Project Goals
+# Как запустить
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5.
+При запуске с ключем `-b` можно указать файл со списком распространенных паролей, который можно взять , например, [отсюда](https://github.com/danielmiessler/SecLists/tree/master/Passwords/Common-Credentials)
+
+Запуск на Linux:
+
+```bash
+
+$ python password_strength.py -b passwords_blacklist.txt
+Enter the personal data that will be used to verify the password: Vasya Pupkin 1990
+Enter password:
+Password strength:  4
+```
+
+Запуск на Windows происходит аналогично.
+
+# Цели проекта
+
+Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
+
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,111 @@
+import re
+import getpass
+import string
+import argparse
 
 
-def get_password_strength(password):
-    pass
+def validate_password(typed_password):
+    if re.search(r'[^ -~]+', typed_password):
+        raise ValueError(
+            'password must contain only English characters,'
+            ' numbers and punctuation'
+        )
+    elif not typed_password:
+        raise ValueError('You should enter password')
+    else:
+        return
+
+
+def get_password_from_keyboard():
+    while True:
+        typed_password = getpass.getpass(prompt='Enter password: ')
+        try:
+            validate_password(typed_password)
+            return typed_password
+        except ValueError as e:
+            print(str(e))
+
+
+def calc_penalty_by_pers_data(typed_password, person_data=None):
+    penalty = 0
+
+    if person_data:
+        for word in re.sub(r'[^a-z]', ' ', person_data.lower()).split():
+            if word in re.sub(r'[^a-z]', '', typed_password.lower()):
+                penalty += len(word)
+
+        for digit in re.sub(r'[\D]', ' ', person_data).split():
+            if digit in re.sub(r'[\D]', '', typed_password):
+                penalty += len(digit)
+
+    return penalty
+
+
+def get_password_strength(typed_password, blacklist=[], person_data=None):
+
+    if typed_password in blacklist:
+        return 1
+
+    score = len(typed_password) - 6
+    if score < 0:
+        score = 0
+    if score > 6:
+        score = 6
+
+    if re.search(r'[' + string.punctuation + ']', typed_password):
+        score += 1
+
+    if re.search(r'[' + string.ascii_uppercase + ']', typed_password):
+        score += 1
+
+    if re.search(r'[' + string.ascii_lowercase + ']', typed_password):
+        score += 1
+
+    if re.search(r'[' + string.digits + ']', typed_password):
+        score += 1
+
+    if person_data:
+        score -= calc_penalty_by_pers_data(typed_password, person_data)
+
+    if score < 1:
+        score = 1
+    if score > 10:
+        score = 10
+    return score
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-b', action='store',
+        dest='blacklist',
+        help='Filepath with passwords blacklist',
+        default=None
+    )
+
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    pass
+    params = parse_arguments()
+
+    if params.blacklist:
+        try:
+            with open(params.blacklist, 'r') as file_handler:
+                blacklist = file_handler.read().split()
+        except (OSError, ValueError):
+            print('failed load blacklist')
+            blacklist = []
+    else:
+        blacklist = []
+
+    person_data = input(
+        'Enter the personal data that will be used to verify the password: '
+    )
+
+    typed_password = get_password_from_keyboard()
+    password_strength = get_password_strength(
+        typed_password, blacklist, person_data
+    )
+    print('Password strength: ', password_strength)
